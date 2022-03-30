11f4b05c6d94e86ed7dc340eb32ecc1955516f31
Jessica Fellows <jessicaxfellows@gmail.com>
2022-03-29 21:49:05 -0400

    Arrays Assignment

---
+++ Basic_C#_Programs/ArraysAssignment/ArraysAssignment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+    class Program
+    {
+        static void Main()
+        {
+        //String array
+        string[] stringArray = { "dog", "cat", "hamster", "zerba" };
+        Console.WriteLine("Please select an index from 0-3:");
+        int value = Convert.ToInt32(Console.ReadLine());
+        if (value > 3)
+        {
+            Console.WriteLine("The number you entered was not between 0-3.");
+        }
+        else
+        {
+            Console.WriteLine(stringArray[value]);
+        }
+
+
+        //Integer array
+        int[] intArray = { 12, 14, 16, 18 };
+        Console.WriteLine("Please select an index from 0-3:");
+        int value1 = Convert.ToInt32(Console.ReadLine());
+        if (value1 > 3)
+        {
+            Console.WriteLine("The number you entered was not between 0-3.");
+        }
+        else
+        { 
+            Console.WriteLine(intArray[value1]);
+        }
+
+        List<string> stringList = new List<string>();
+        stringList.Add("Jess");
+        stringList.Add("Jp");
+        stringList.Add("Nami");
+        stringList.Add("Sanji");
+        Console.WriteLine("Please select an index from 0-3:");
+        int value2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(stringList[value2]);
+
+        Console.ReadLine();
+        }
+    }
+
