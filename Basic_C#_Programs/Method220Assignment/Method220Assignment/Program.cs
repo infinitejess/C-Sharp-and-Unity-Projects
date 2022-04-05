b8b4c0a1687ae9f117fda16205470e09d10be74d
Jessica Fellows <jessicaxfellows@gmail.com>
2022-04-05 13:03:13 -0400

    Method 220 Assignment

---
+++ Basic_C#_Programs/Method220Assignment/Method220Assignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method220Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwoMethod add = new TwoMethod(); //instantiate the class
+            
+            Console.WriteLine("Enter a number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());  //ask for the first number 
+
+            Console.WriteLine("Enter a 2nd number, this is optional (hit enter either way):"); //ask for the second number but optional
+            int num2; 
+            if (int.TryParse(Console.ReadLine(), out num2)) //now check IF they entered a second number
+            {
+                Console.WriteLine(add.twoNums(num1, num2)); //if they did, print the result with num1 and num2
+            }
+            else
+            {
+                Console.WriteLine(add.twoNums(num1)); //else print the result with num1 and default of optional parameter
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
