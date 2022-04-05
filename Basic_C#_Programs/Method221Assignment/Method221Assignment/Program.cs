f8c54a26105984c498b7da31718290874682684f
Jessica Fellows <jessicaxfellows@gmail.com>
2022-04-05 17:54:28 -0400

    Method 221 Assignment

---
+++ Basic_C#_Programs/Method221Assignment/Method221Assignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method221Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method221 example = new Method221();
+            int x = 10;
+            int y = 15;
+            example.classOne(x, y);
+            
+
+            Console.ReadLine();
+
+        }
+    }
+}
