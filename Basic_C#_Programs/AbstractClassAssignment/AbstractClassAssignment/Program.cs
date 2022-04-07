03609e8b350c711cf426d606f90721df1fdf580e
Jessica Fellows <jessicaxfellows@gmail.com>
2022-04-07 15:29:07 -0400

    Abstract Classs Assignment

---
+++ Basic_C#_Programs/AbstractClassAssignment/AbstractClassAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee newEmp = new Employee();
+            newEmp.firstName = "Sample";
+            newEmp.lastName = "Student";
+            newEmp.SayName();
+
+            Console.ReadLine();
+        }
+    }
+}
