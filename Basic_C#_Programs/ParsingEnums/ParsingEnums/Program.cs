f06e1ab225061b09674c09bd3660b5f7180ce248
Jessica Fellows <jessicaxfellows@gmail.com>
2022-04-08 16:48:00 -0400

    Parsing Enums

---
+++ Basic_C#_Programs/ParsingEnums/ParsingEnums/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnums
+{
+    class Program
+    {
+        public enum daysOfWeek
+        { 
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What day of the week is it?");
+            string dayInput = Console.ReadLine();
+            daysOfWeek day;
+
+            try
+            {
+                day = (daysOfWeek)Enum.Parse(typeof(daysOfWeek), dayInput);
+                Console.WriteLine("Good job!");
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week. (Hint: Capitalize the first letter.)");
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
